3bcafdd8321aba794557d2f22f9d32c69d052765
elektrycznyy <elektrycznyy@users.noreply.github.com>
2020-12-20 16:06:34 +0100

    Dodanie drugiego imienia

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,8 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Mateusz"
-moje_imie2 = "Dominik"
+moje_imie = "MateuszW"
 msg = "Hello World!"
 
 @app.route('/')
@@ -12,7 +11,7 @@ def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie, moje_imie2,
+    return get_formatted(msg, moje_imie,
                          output.lower())
 
 @app.route('/outputs')
